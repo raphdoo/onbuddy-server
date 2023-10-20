f0d5ba3bba6b9673c66649e2f2533cd64c45771b
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-10-20 10:36:39 +0100

    implementing encrypted password services

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface that describes the properties required to create a new user
 interface UserAttrs {
@@ -124,8 +125,8 @@ const userSchema = new mongoose.Schema(
 //presave function to update to hashed password
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
-    // const hashed = await Password.toHash(this.get('password'));
-    // this.set('password', hashed);
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
   }
 
   done();
