839b0fd66c62ab3be2c482afdceb713ab57b38b2
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-10-21 17:44:59 +0100

    fix:undefined env variable log

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
-import { app } from "./app";
-import config from "./src/common/config";
-import { envVars } from "./src/common/constants";
+import { app } from './app';
+import config from './src/common/config';
+import { envVars } from './src/common/constants';
 
-import connectDatabase from "./src/common/dbConnect";
+import connectDatabase from './src/common/dbConnect';
 
 const port: number = config.app.port;
 // Mongodb setup
 const start = async () => {
-  envVars.forEach((each) => console.log(process.env[each]));
+  envVars.forEach(
+    (each) => !process.env[each] && console.log(each, 'is not defined')
+  );
 
-  connectDatabase();
+  try {
+    connectDatabase();
+  } catch (err) {
+    throw new Error('error connecting to database');
+  }
 };
 
 app.listen(port, () => {
