d5dcbc01c2b7d8169935a75fa9f84796377de4e5
Skywonda <princesam3600@gmail.com>
2023-10-21 16:05:19 +0100

    fix: test cases fix

--- src/api/auth/__test__/currentuser.test.ts
+++ src/api/auth/__test__/currentuser.test.ts
@@ -1,21 +1,21 @@
-import request from 'supertest';
-import { app } from '../../../../app';
+import request from "supertest";
+import { app } from "../../../../app";
 
-it('returns with details about the current user', async () => {
+it("returns with details about the current user", async () => {
   const cookie = await global.signup();
 
   const response = await request(app)
-    .get('/api/v1/users/currentuser')
-    .set('Cookie', cookie)
+    .get("/api/v1/auth/currentuser")
+    .set("Cookie", cookie)
     .send()
     .expect(200);
 
-  expect(response.body.currentUser.email).toEqual('test@test.com');
+  expect(response.body.currentUser.email).toEqual("test@test.com");
 });
 
-it('responds with null if not authenticated', async () => {
+it("responds with null if not authenticated", async () => {
   const response = await request(app)
-    .get('/api/v1/users/currentuser')
+    .get("/api/v1/auth/currentuser")
     .send()
     .expect(200);
 
