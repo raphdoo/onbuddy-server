7e50e70db5d39c32d1a92f010e8a4833e40b9134
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-10-21 20:32:33 +0100

    feat:add verify-company-id middleware

--- src/api/auth/currentuser.ts
+++ src/api/auth/currentuser.ts
@@ -1,10 +1,10 @@
-import express, { Response, Request } from "express";
-import { currentUser } from "../../../middlewares/current-user";
-import { adminUser } from "../../../middlewares/admin-user";
+import express, { Response, Request } from 'express';
+import { currentUser } from '../../../middlewares/current-user';
+import { adminUser } from '../../../middlewares/admin-user';
 
 const router = express.Router();
 
-router.get("/currentuser", currentUser, (req: Request, res: Response) => {
+router.get('/currentuser', (req: Request, res: Response) => {
   res.send({ currentUser: req.currentUser || null });
 });
 
