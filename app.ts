453b78168f8f3a0ff4e718513c5d815a31693878
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-11-15 16:35:38 +0100

    fix: file size

--- app.ts
+++ app.ts
@@ -26,7 +26,8 @@ const app = express();
 app.set('trust proxy', true);
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
 // app.use(
